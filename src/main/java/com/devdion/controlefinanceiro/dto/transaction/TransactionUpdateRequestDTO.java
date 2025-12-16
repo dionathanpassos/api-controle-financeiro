6a678ca3229c4aed88d5ca3a504149649d4aa3ab
Dionathan Passos <devdionathanpassos@gmail.com>
2025-12-15 21:17:17 -0300

    feat: add validação-notação DTO Update

--- src/main/java/com/devdion/controlefinanceiro/dto/transaction/TransactionUpdateRequestDTO.java
+++ src/main/java/com/devdion/controlefinanceiro/dto/transaction/TransactionUpdateRequestDTO.java
@@ -1,16 +1,28 @@
 package com.devdion.controlefinanceiro.dto.transaction;
 
 import com.devdion.controlefinanceiro.model.TransactionType;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record TransactionUpdateRequestDTO(
+
+        @NotNull
         Long accountId,
+
+        @NotNull
         Long categoryId,
+
+        @NotNull
         BigDecimal amount,
+
+        @NotNull
         TransactionType type,
+
         String description,
+
+        @NotNull
         LocalDate date
 ) {
 }
